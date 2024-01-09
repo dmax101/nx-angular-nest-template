6520e6891244756eb55a9ef1d43785d3cb3c2559
Danilo Ribeiro <danriba@gmail.com>
2024-01-09 17:53:38 -0300

    adjust main.ts and launch.json

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,18 +12,20 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Basic API NX Angular Nest')
+    .setDescription('Template for fast development!')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('template')
+    .setBasePath('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
