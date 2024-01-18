1027dd8983fb31ee903e87a444bee890bce6a977
Danilo Ribeiro <danriba@gmail.com>
2024-01-18 00:06:59 -0300

    fix dark button and apply reactive forms

--- apps/nx-angular-nest-template/src/app/core/auth/login.component.ts
+++ apps/nx-angular-nest-template/src/app/core/auth/login.component.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { ThemeService } from '../../shared/services/theme.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'nx-angular-nest-template-login',
   standalone: true,
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -15,9 +21,19 @@ export class LoginComponent implements OnInit, OnDestroy {
   isDarkMode$ = new BehaviorSubject<boolean>(false);
   isDarkModeSubscription!: Subscription;
 
-  constructor(private themeService: ThemeService) {}
+  loginForm!: FormGroup;
+
+  constructor(
+    private themeService: ThemeService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]],
+    });
+
     this.isDarkModeSubscription = this.themeService.isDarkMode.subscribe(
       (data) => {
         this.isDarkMode$.next(data);
@@ -27,5 +43,9 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.isDarkModeSubscription.unsubscribe();
+  }
+
+  onSubmit() {
+    this.loginForm.reset();
   }
 }
